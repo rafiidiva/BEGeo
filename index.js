68dd2f7fd91d69cf08764cd7c5e959a87ff1f3b5
Rafi <rafiisephana@gmail.com>
2021-04-15 16:24:32 +0700

    menambahkan fastify-static dan nodemon,menambahkan public folder

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 // Require the framework and instantiate it
-const fastify = require('fastify')({ logger: true })
+const fastify = require('fastify')({ logger: true });
+const path = require('path');
+
+//Register plugins
+fastify.register(require('fastify-static'), {
+  root: path.join(__dirname, 'public'),
+  prefix: '/', // optional: default '/'
+});
+fastify.register(require('fastify-static'), {
+  root: path.join(__dirname, 'public/assets'),
+  prefix: '/assets',
+  decorateReply: false
+});
+fastify.register(require('fastify-static'), {
+  root: path.join(__dirname, 'public/forms'),
+  prefix: '/forms',
+  decorateReply: false
+});
+// Declare a route
+fastify.get('/', async (request, reply) => {
+  reply.sendFile('index.html');// serving path.join(__dirname, 'public', 'index.html') directly
+});
 
 // Declare a route
 fastify.get('/apa', async (request, reply) => {
   return { hello: 'apa' };
-})
+});
 
 // Run the server!
 const start = async () => {
