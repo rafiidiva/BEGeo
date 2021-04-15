52efae31d91050f63d58da7ec38746c39b050c0f
Rafi <rafiisephana@gmail.com>
2021-04-15 17:11:55 +0700

    memindahkan import path package

--- config/static.js
+++ config/static.js
@@ -1,15 +1,18 @@
+const path = require('path');
+
 module.exports={
     public: {
-    root: path.join(__dirname,"public"),
-    prefix:"/"  
+     root: path.join(__dirname,"public"),
+     prefix:"/"  
     },
     assets: {
-        root: path.join(__dirname, 'public/assets'),
-    prefix: '/assets'
+     root: path.join(__dirname, 'public/assets'),
+     prefix: '/assets',
+     decorateReply: false,
     },
     forms: {
-        root: path.join(__dirname, 'public/forms'),
-    prefix: '/forms',
-  decorateReply: false
+     root: path.join(__dirname, 'public/forms'),
+     prefix: '/forms',
+     decorateReply: false
     },
 };
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Require the framework and instantiate it
 const fastify = require('fastify')({ logger: true });
-const path = require('path');
+
 
 //Register plugins
 fastify.register(require('fastify-static'), require("./config/static").public);
